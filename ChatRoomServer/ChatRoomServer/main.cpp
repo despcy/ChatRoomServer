04b803fb2b01d645aae27ea692801d70febabbf6
YOUNG <ycx@live.cn>
2019-02-19 12:28:05 -0800

    xcode

---
+++ ChatRoomServer/ChatRoomServer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ChatRoomServer
+//
+//  Created by ycx on 2019/2/19.
+//  Copyright © 2019 chenxiy.net. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Chat room server side for EECS22L project
 
 Reference: https://cs.baylor.edu/~donahoo/practical/CSockets/practical/
+
+Reference: https://blog.csdn.net/lovekun1989/article/details/41042273
